38f013ad98347b8ae39af208a7d7e4c1291608e1
quandarealest <buikhacminhquan@hotmail.com>
2022-07-19 19:05:22 -0400

    change localhost to 127.0.0.1

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ const server = http.createServer(app)
 app.use(cors())
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://127.0.0.1:3000",
     methods: ["GET", "POST"]
   },
 })
